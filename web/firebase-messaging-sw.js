1f5dfe15b0dda73d58586983b27b6e64f5dbda83
Tahil Bansal <Tahil.Bansal@target.com>
2025-01-10 21:24:39 +0530

    Vercel env manage

---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "undefined",
+    authDomain: "undefined",
+    databaseURL: "undefined",
+    projectId: "undefined",
+    storageBucket: "undefined",
+    messagingSenderId: "undefined",
+    appId: "undefined",
+    measurementId: "undefined"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/firebase-logo.png'
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
